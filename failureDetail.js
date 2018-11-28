472ac167f187784273c56aba4df99b374004a808
Jeff Mcmahan <jeff@Jeffs-MacBook-Pro.local>
2018-11-28 09:54:04 -0500

    initial

---
+++ failureDetail.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const typeCheck = require('./typeCheck')
+
+module.exports = (value, expectedType) => {
+	if (typeCheck(value, expectedType)) {
+		return null
+	}
+
+	const expectedTypeName = expectedType.hmac 
+		? expectedType.__desc 
+		: expectedType.name
+	
+	const valueTypeName = (
+			value == null ? value
+		: 	value.constructor ? value.constructor.name
+		:	value.constructor
+	)
+
+	return {expectedTypeName, valueTypeName}
+}
